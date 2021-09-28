90dac88cb32f0a7aac0d450a2c76d8b68d88c066
Taras Kuran <taras@example.com>
2021-09-28 19:20:30 +0300

    0.1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 $(window).on("load" , function () {
+    new fullpage('#fullpage', {
+        
+    })
+    //$('#fullpage').fullpage();
     $('.hero').addClass('hero_active')
     $('.header').addClass('header_active')
 
+
+
+    $(window).on('mousewheel', () => {
+        if ($('.hero').hasClass('active')) {
+            $('body').removeClass('color')
+            $('.hero').addClass('hero_active')
+        } else {
+            $('body').addClass('color')
+            $('.hero').removeClass('hero_active')
+        }
+    })
 })
 
 
